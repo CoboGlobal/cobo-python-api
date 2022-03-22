37bb80179305b7069af495218723b899a64e5ce5
caixiao-QA <caixiao@cobo.com>
2022-03-22 19:05:14 +0800

    update readme

--- test.py
+++ test.py
@@ -309,8 +309,9 @@ class ClientTest(unittest.TestCase):
 
 if __name__ == '__main__':
     if len(sys.argv) > 1:
-        env =  sys.argv[0]
-        api_secret = sys.argv[1]
+        api_secret = sys.argv.pop()
+        env =  sys.argv.pop()
+
 
         ClientTest.api_secret = api_secret
         ClientTest.ENV = SANDBOX_ENV if env == "sandbox" else PROD_ENV

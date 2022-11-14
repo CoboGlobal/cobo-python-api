9d014ed9547a4bda1de8ceae318edf4a07a742ce
caixiao-QA <caixiao@cobo.com>
2022-11-14 16:42:29 +0800

    update setup

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 setup(
     name="cobo_custody",
-    version="0.15",
+    version="0.16",
     author="Cobo",
     author_email="support@cobo.com",
     description="Cobo Custody restful api",
@@ -10,7 +10,7 @@ setup(
     license="Cobo Copyright Reserved",
     python_requires=">=3.7",
     url="https://github.com/CoboCustody/cobo-python-api",
-    packages=['cobo_custody', 'cobo_custody.signer', 'cobo_custody.client', 'cobo_custody.error', 'cobo_custody.config'],
+    packages=['cobo_custody', 'cobo_custody.model','cobo_custody.signer', 'cobo_custody.client', 'cobo_custody.error', 'cobo_custody.config'],
     include_package_data=True,
     install_requires=["ecdsa==0.17.0", "requests"]
     # zip_safe=False,

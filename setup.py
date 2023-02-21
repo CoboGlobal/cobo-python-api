0292a0e4d9b0bb5c4c14e35477b4ea8f7ef04f52
zhanghuiwen <zhanghuiwen@zhanghuiwendeMacBook-Pro.local>
2023-02-21 12:32:07 +0800

    update release info

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 setup(
     name="cobo_custody",
-    version="0.19",
+    version="0.20",
     author="Cobo",
     author_email="support@cobo.com",
     description="Cobo Custody restful api",

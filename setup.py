fb09d87febec4791ef0d3bc09cf35f8e1d330a83
Ming Yang <ming.yang@cobo.com>
2024-06-03 11:24:14 +0800

    update release info

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 setup(
     name="cobo_custody",
-    version="0.38",
+    version="0.39",
     author="Cobo",
     author_email="support@cobo.com",
     description="Cobo Custody restful api",
